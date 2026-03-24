f295353d41918720ced2301648a8518f491ce67a
Harsh Jangra <jsam44dev@gmail.com>
2026-03-24 18:03:25 +0530

    sew

--- Arrays/src/Searching/BinarySearch.java
+++ Arrays/src/Searching/BinarySearch.java
@@ -3,8 +3,9 @@ package Searching;
 public class BinarySearch {
     public static void main(String[] args) {
         int []arr= {2,4,6,9,11,12,14,20,36,48};
-        int find=37;
+        int find=48;
         System.out.println(Binary(arr,find));
+        System.out.println(Recursion(arr,find,0,arr.length-1));
 
     }
     static int Binary(int[]arr,int target){
@@ -22,6 +23,22 @@ public class BinarySearch {
                 return mid;
             }
         }
-        return arr[end];
+        return -1;
+        // Time Complexity: O(log n) base 2
+        // Space Complexity: O(1)
+    }
+    static int Recursion(int[] arr, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        int mid = start + (end - start) / 2; // to avoid overflow case of Integer
+        if (target > arr[mid]) {
+            return Recursion(arr, target, mid + 1, end);
+        } else if (target < arr[mid]) {
+            return Recursion(arr, target, start, mid - 1);
+        }
+        return mid;
+        // Time Complexity: O(log n) base 2
+        // Space Complexity: O(log n)
     }
 }
